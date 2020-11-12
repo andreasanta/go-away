1d50043c4ce3db8b3ad6c8c00e27de3cfa9a0f0b
Andrea <andrea@yondbee.com>
2020-11-12 02:45:48 +0100

    Implemented IP lookup via hashmap

--- internal/http/ip_endpoint.go
+++ internal/http/ip_endpoint.go
@@ -1,21 +1,47 @@
 package http
 
 import (
-	"github.com/gin-gonic/gin"
+	"encoding/binary"
+	"errors"
 	"log"
+	"net"
+
+	"github.com/gin-gonic/gin"
 )
 
 type Response struct {
 	Status    string `json:"status"`
-	Reason    string `json:"description,omitempty"`
-	Timestamp uint   `json:"timestamp,omitempty"`
+	List      string `json:"description,omitempty"`
+	Timestamp int64  `json:"timestamp,omitempty"`
+}
+
+func ip2int(ip net.IP) uint32 {
+	return binary.BigEndian.Uint32(ip.To4())
 }
 
 func GetIp(c *gin.Context) {
-	
-	log.Println("Inside Get IP")
 
-	c.JSON(200, Response{
-		Status: "ok",
-	})
+	log.Println("Inside Get IP %s", c.Param("ip"))
+	ip := net.ParseIP(c.Param("ip"))
+
+	if ip == nil {
+		c.AbortWithError(400, errors.New("IP Not valid"))
+		return
+	}
+	log.Printf("DWORD IP conversion %d", ip2int(ip))
+
+	susIp, ok := FindSuspiciousIp(ip2int(ip))
+
+	if !ok {
+		c.JSON(200, Response{
+			Status: "ok",
+		})
+	} else {
+		c.JSON(200, Response{
+			Status:    "ko",
+			List:      susIp.List,
+			Timestamp: susIp.Timestamp.Unix(),
+		})
+	}
+
 }
--- internal/http/server.go
+++ internal/http/server.go
@@ -1,14 +1,64 @@
 package http
 
 import (
+	"goaway/internal/models"
+	"goaway/internal/utils"
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
+
+var IpList map[uint32]models.IPRange
+
+func loadDatabase() {
+
+	log.Println("Loading database, will take a long while")
+
+	db, err := gorm.Open(sqlite.Open(os.Getenv("DB_FILE_PATH")), &gorm.Config{
+		SkipDefaultTransaction: true,
+		Logger:                 logger.Default.LogMode(logger.Silent),
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	IpList = make(map[uint32]models.IPRange)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var results []models.IPRange
+	db.Table("ip_ranges").Where("end = ?", 0).FindInBatches(&results, 10000, func(tx *gorm.DB, batch int) error {
+		for _, result := range results {
+			// batch processing found records
+			IpList[result.Start] = result
+		}
+		return nil
+	})
+
+	log.Printf("Loaded DB rows %d", len(IpList))
+
+}
+
+func FindSuspiciousIp(ip uint32) (models.IPRange, bool) {
+
+	log.Printf("Loaded DB rows %d", len(IpList))
+
+	val, ok := IpList[ip]
+	return val, ok
+}
 
 func RunServer() {
 
-	loadConfig()
+	utils.LoadConfig()
+
+	loadDatabase()
 
 	r := gin.Default()
 
